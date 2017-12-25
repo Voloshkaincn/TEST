5cbd4aa3bf26b53c2eac99b4f296cb5b55b1693f
voloshkaincn <barvinok.cn@gmail.com>
2017-12-25 06:20:02 +0200

    first commit

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,14 @@
+head: {
+  ru: {
+    defaults: {
+      title: '354 Exclusive Height',
+      useSocialMetaTags: true
+    }
+  },
+  en: {
+    defaults: {
+      title: '354 Exclusive Height',
+      useSocialMetaTags: true
+    }
+  }
+}
